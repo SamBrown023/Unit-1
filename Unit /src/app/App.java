1f92ce9bde1a956a8d57c8bdd99cf4538487c401
SamBrown023 <817815@mystma.org>
2020-04-08 15:32:38 +0000

    Added top five favorite movies

--- Unit /src/app/App.java
+++ Unit /src/app/App.java
@@ -2,6 +2,10 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("1. The Adventures of Tintin");
+        System.out.println("2. Rope");
+        System.out.println("3. Clue");
+        System.out.println("4. We're No Angels");
+        System.out.println("5. Hugo");
     }
 }
