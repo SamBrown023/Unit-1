a53b8bdf96e74320b3737cf70fd9e9756c1f2470
SamBrown023 <817815@mystma.org>
2020-04-08 17:50:24 +0000

    Added top five favorite movies

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,7 +5,7 @@ public class App {
         System.out.println("1. The Adventures of Tintin");
         System.out.println("2. Rope");
         System.out.println("3. Clue");
-        System.out.println("4. We're No Angels");
+        System.out.println("4. We're No Angels (1955)");
         System.out.println("5. Hugo");
     }
 }
