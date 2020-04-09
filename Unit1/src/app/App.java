8b0ca8b58b59a2c5e3821aa18f4f6907e382fa56
SamBrown023 <817815@mystma.org>
2020-04-09 16:22:20 +0000

    added 8 variables

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,10 +2,30 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("1. The Adventures of Tintin");
-        System.out.println("2. Rope");
-        System.out.println("3. Clue");
-        System.out.println("4. We're No Angels (1955)");
-        System.out.println("5. Hugo");
+        boolean livesInAlbertville = false; 
+        char frstNameInitial = 'S';
+        byte lettersInName = 16;
+        short randmNumbr = 27394;
+        int searchResultsForSTMAHighSchool = 280000;
+        long secondRandmNumbr = -8827357235541564542L;
+        float fivedividbytwo = 2.5F;
+        double eightdividbysevn = 1.14285714286;
+
+        System.out.println("Do I live in Albertville?");
+        System.out.println(livesInAlbertville);
+        System.out.println("What is the initial of my first name?");
+        System.out.println(frstNameInitial);
+        System.out.println("How many letters are in my name?");
+        System.out.println(lettersInName);
+        System.out.println("Tell me a random number");
+        System.out.println(randmNumbr);
+        System.out.println("How many search results are there for 'STMA highschool'?");
+        System.out.println(searchResultsForSTMAHighSchool);
+        System.out.println("Tell me another random number");
+        System.out.println(secondRandmNumbr);
+        System.out.println("What is five divided by three?");
+        System.out.println(fivedividbytwo);
+        System.out.println("What is eight divided by seven?");
+        System.out.println(eightdividbysevn);
     }
 }
