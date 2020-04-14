8badc290109ad32807862c57c58b5440479bf328
SamBrown023 <817815@mystma.org>
2020-04-14 14:57:44 +0000

    added while loops

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,18 +2,33 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception { 
-        String place1 = "the park ";
-        String place2 = "school ";
-        String adjective1 = "cold ";
-        String adjective2 = "warm "; 
-        String person1 = "Sam ";
-        String person2 = "Emily ";
-        String verb1 = "looking ";
-        String verb2 = "dance ";
-        String verb3 = "sit ";
-        String food = "pancakes ";
-        String dayOfWeek = "Thursday ";
+        int count = 0;
+        String color = "red";
+        boolean trueOrFalse = false;
+
+        while (count < 10){
+            System.out.println("The count is " + count);
+            count = count + 1;
+
+        }
         
-        System.out.println(person1 + " is a programmer who likes to " + verb1 + ". " + "Every " + dayOfWeek + "they make a " + adjective1 + food + "for breakfast and " + verb2 + "at " + place1 + "with " + person2 + ". " + "Sometimes, they go to the " + adjective2 + place2 + "with " + person2 + "so that they can " + verb3 + "togehter.");
+        System.out.println("Count Finished");
+
+
+        while (color == "red"){
+            System.out.println("The color is " + color);
+            color = "green";
+        }
+
+        System.out.println("Color finished");
+
+
+        while (trueOrFalse == false){
+            System.out.println("The answer is " + trueOrFalse);
+            trueOrFalse = true;
+        }
+
+        System.out.println("True or False Finished");
+
     }
 }
