37366919ea1171ce2c3bbb92845efd52f79c18f5
SamBrown023 <817815@mystma.org>
2020-04-15 15:40:23 +0000

    added for loops

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,33 +2,26 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception { 
-        int count = 0;
-        String color = "red";
-        boolean trueOrFalse = false;
+        for (int i=10; i<=20; i=i+1){
+            System.out.println("The count is " + i);
+        }
 
-        while (count < 10){
-            System.out.println("The count is " + count);
-            count = count + 1;
+        for (int i=10; i<=100; i=i+1){
+            System.out.println("The count is " + i);
+        }
 
+        for (int i=1000; i<=256000; i=i+1){
+            System.out.println("The count is " + i);
+        }
+
+        for (int i=11; i<= 12; i=i+1){
+            System.out.println("The count is " + i);
+        }
+
+        for (int i=10; i>=0; i=i-1){
+            System.out.println("The count is " + i);
         }
         
-        System.out.println("Count Finished");
-
-
-        while (color == "red"){
-            System.out.println("The color is " + color);
-            color = "green";
-        }
-
-        System.out.println("Color finished");
-
-
-        while (trueOrFalse == false){
-            System.out.println("The answer is " + trueOrFalse);
-            trueOrFalse = true;
-        }
-
-        System.out.println("True or False Finished");
 
     }
 }
