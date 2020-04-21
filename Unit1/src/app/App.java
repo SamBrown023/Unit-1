0b9ced281e9160b543fa41c02b53230a8c459883
SamBrown023 <817815@mystma.org>
2020-04-21 19:32:56 +0000

    mastering if statements

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,16 +2,39 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception { 
-        
-        for (int i=1; i<=20; i=i+1){
-            if (i % 4 == 0){
-                System.out.println(i + " is divisible by 4");
+        System.out.println("Start first loop");
+
+        for (int i=0; i<=22; i=i+1){
+
+            for (int x=0; x<=9; x=x+0)
+            System.out.println(x);
+
+            if (i>=16){
+                    System.out.println("10");
+                }
+                else{
+                    System.out.println(i);
+                }
             }
-            else{
-                System.out.println(i + " is not divisible by 4");
-            } 
 
+        System.out.println("End first loop");
+        System.out.println("Start Second loop");
+
+       for (int i=0; i<=13; i=i+1){
+        
+          if (i%2==0 ){
+              System.out.println(i-1+(i/2));
+             }
+        else System.out.println((i*2)/3);
+            
         }
-
     }
 }
+    
+        
+        
+
+
+        
+
+    
