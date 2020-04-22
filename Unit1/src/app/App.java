c908088190ae03078ca2905388c3d1709dcf1088
SamBrown023 <817815@mystma.org>
2020-04-22 19:17:47 +0000

    mastering if statements pt2

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,38 +3,25 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception { 
         System.out.println("Start first loop");
-
-        for (int i=0; i<=22; i=i+1){
-
-            for (int x=0; x<=9; x=x+0)
-            System.out.println(x);
-
-            if (i>=16){
-                    System.out.println("10");
-                }
-                else{
-                    System.out.println(i);
+        
+        for (int i=20; i>=1; i=i-1){
+            System.out.println(i);
+            if (i==10){
+                for (int j=0; j<7; j++){
+                System.out.println(i);
                 }
             }
-
-        System.out.println("End first loop");
-        System.out.println("Start Second loop");
-
-       for (int i=0; i<=13; i=i+1){
-        
-          if (i%2==0 ){
-              System.out.println(i-1+(i/2));
-             }
-        else System.out.println((i*2)/3);
-            
         }
+        
+        System.out.println("End first loop");
+        System.out.println("Start second loop");
+
+        for (int i=0; i<=9; i=i+1){
+            if (i%3==1){
+                System.out.println("Hey");
+            }
+            System.out.println((i+(2*i)+(i-(i*2)))*2);
+        }
+        System.out.println("End second loop");
     }
 }
-    
-        
-        
-
-
-        
-
-    
