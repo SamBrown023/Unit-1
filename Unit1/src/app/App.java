2f8f676ea797faf7d6ef2ca6cd56e11d5ace09a5
SamBrown023 <817815@mystma.org>
2020-04-24 14:53:31 +0000

    Quiz 1

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,27 +2,31 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Double or single digit?");
-
-        for (int i = 20; i >= 1; i = i - 1) {
-            if (i >= 10) {
-                System.out.println(i + " is double digit");
-            } else {
-                System.out.println(i + " is single digit");
-
+        
+        System.out.println("Start first loop");
+        
+        for (int i=1; i<=10; i=i+1){
+            if (i==5){
+                System.out.println("five");
             }
-        }
-        System.out.println("Nice!");
+            else {
+                System.out.println(i);
 
-        System.out.println("Fast food");
-
-        for (int i = 1; i <= 10; i = i + 1)
-            if (i % 2 == 0) {
-                System.out.println("Fries");
-            } else {
-                System.out.println("Burgers");
-            }
-
-        System.out.println("Nice!");
     }
+}
+
+        System.out.println("End first loop");
+        System.out.println("Start second loop");
+
+        for (int i=100; i<=200; i=i+10){
+            System.out.println(i);
+            if (i==150){
+                for (int j=151; j<=159; j=j+1)
+                System.out.println(j);
+            }
+
+        }
+
+        System.out.println("End second loop");
+}
 }
