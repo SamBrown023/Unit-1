9bc87e3a66867c69e9e34fbfe5f267d1aafbb902
SamBrown023 <817815@mystma.org>
2020-05-05 15:00:25 +0000

    added quiz

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,13 +6,35 @@ public class App {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please enter a number");
-        int number = input.nextInt();
+        System.out.println("What is 2 x 4?");
+        String math = input.nextLine();
+        if (math.equals("8")){
+            System.out.println("Correct!");
+        }
+        else {
+            System.out.println("Incorrect");
+        }
 
-        for (int i=1; i<=number; i=i+1){
-            System.out.println(i);
+        System.out.println("Who wrote 'Gulliver's Travels'?");
+        String lit = input.nextLine();
+        if (lit.equals("Jonathan Swift")){
+            System.out.println("Correct!");
+        }
+        else{
+            System.out.println("Incorrect");
+        }
+
+        System.out.println("Who started the Protestant Revolution by famously nailing 95 theses to a church door?");
+        String hist = input.nextLine();
+        if (hist.equals("Martin Luther")){
+            System.out.println("Correct!");
+        }
+        else{
+            System.out.println("Incorrect");
+        }
+
+
         }
         
 
     }
-}
