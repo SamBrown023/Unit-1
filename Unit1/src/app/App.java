b10003d688f3ff2b15c0947d835ae0c0b3ab2571
SamBrown023 <817815@mystma.org>
2020-05-08 17:15:11 +0000

    calculator program

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,41 +6,31 @@ public class App {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please enter a name");
-        String name1 = input.nextLine();
+        System.out.println("Please enter 1st number");
+        int numb1 = input.nextInt();
 
-        System.out.println("Please enter a 2nd name");
-        String name2 = input.nextLine();
+        System.out.println("Please enter 2nd number");
+        int numb2 = input.nextInt();
 
-        System.out.println("Please enter a 3rd name");
-        String name3 = input.nextLine();
+        System.out.println("Please enter 1 to add, 2 to subtract, 3 to multiply, and 4 to divide");
+        int func = input.nextInt();
 
-        System.out.println("Please enter an adjective");
-        String adj1 = input.nextLine();
+        if (func == (1)){
+            System.out.println(numb1+numb2);
+        }
 
-        System.out.println("Please enter a 2nd adjective");
-        String adj2 = input.nextLine();
+        if (func == (2)){
+            System.out.println(numb1-numb2);
+        }
 
-        System.out.println("Please enter a 3rd adjective");
-        String adj3 = input.nextLine();
+        if (func == (3)){
+            System.out.println(numb1*numb2);
+        }
 
-        System.out.println("Please enter a noun");
-        String noun1 = input.nextLine();
-
-        System.out.println("Please enter a 2nd noun");
-        String noun2 = input.nextLine();
-
-        System.out.println("Please enter a 3rd noun");
-        String noun3 = input.nextLine();
-
-        System.out.println("Please enter a place");
-        String place = input.nextLine();
-
-        System.out.println("Please enter an action");
-        String action = input.nextLine();
-
-        System.out.println(name1 + " was a little " + adj1 + " " + noun1 + ". One day, they traveled to the " + place + " to " + action + " with their friends. " + name1 + "'s friends were " + name2 + ", a " + adj2 + " " + noun2 + ", and " + name3 + ", a " + adj3 + " " + noun3 + ". After they were done, " + name1 + ", " + name2 + ", and " + name3 + " said goodbye and went home.");
-    }
+        if (func == (4)){
+            System.out.println(numb1/numb2);
+        }
+}
 }
 
 
