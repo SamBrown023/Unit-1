2d136aa1f4f78b71df909fe09fed4005a3879d3e
SamBrown023 <817815@mystma.org>
2020-06-02 14:30:27 +0000

    bank program

---
+++ Unit1/src/app/Account.java
@@ -0,0 +1,8 @@
+package app;
+public class Account{
+
+    String name;
+    String password;
+    int balance;
+
+}
--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,39 +1,90 @@
 package app;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Dog dog1 = new Dog();
-        Dog dog2 = new Dog();
-        Dog dog3 = new Dog();
+        Account account1 = new Account();
+
+        Scanner accountName = new Scanner(System.in);
+        Scanner accountPassword = new Scanner(System.in);
+        Scanner accountBalance = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        Scanner inputComm = new Scanner(System.in);
         
-        dog1.name = "Tigger";
-        dog1.breed = "border collie mix";
-        dog1.age = 5;
-        dog1.gender = "He";
-        dog1.furColor = "black and white";
-        dog1.eyeColor = "brown";
+        System.out.println("To create an account, please enter your name");
+        account1.name = accountName.nextLine();
 
-        dog2.name = "Ringo";
-        dog2.breed = "boxer red heeler mix";
-        dog2.age = 10;
-        dog2.gender = "She";
-        dog2.furColor = "brown and white";
-        dog2.eyeColor = "brown";
+        System.out.println("Please enter password");
+        account1.password = accountPassword.nextLine();
 
-        dog3.name = "Stamps";
-        dog3.breed = "corgi";
-        dog3.age = 3;
-        dog3.gender = "He";
-        dog3.furColor = "brown and white";
-        dog3.eyeColor = "blue";
+        System.out.println("How much money would you like to deposit?");
+        account1.balance = accountBalance.nextInt();
 
-        System.out.println("Dog 1 is named " + (dog1.name) + ". " + (dog1.gender) + " is a " + (dog1.breed) + " and is " + dog1.age + " years old. " + (dog1.gender) + " has " + (dog1.furColor) + " fur and " + (dog1.eyeColor) + " eyes.");
-        System.out.println("Dog 2 is named " + (dog2.name) + ". " + (dog2.gender) + " is a " + (dog2.breed) + " and is " + dog2.age + " years old. " + (dog2.gender) + " has " + (dog2.furColor) + " fur and " + (dog2.eyeColor) + " eyes.");
-        System.out.println("Dog 3 is named " + (dog3.name) + ". " + (dog3.gender) + " is a " + (dog3.breed) + " and is " + dog3.age + " years old. " + (dog3.gender) + " has " + (dog3.furColor) + " fur and " + (dog3.eyeColor) + " eyes.");
+        System.out.println("Thank you for creating an account. Please log in");
+        System.out.println("Please enter your name");
+        String inputName = input.nextLine();
 
-}
-}
+        if (inputName.equals(account1.name)) {  
 
+            System.out.println("Please enter your password");
+            String inputPassword = input.nextLine();
+
+            if (inputPassword.equals(account1.password)) {
+
+                System.out.println("Welcome " + account1.name);
+                System.out.println("What would you like to do?");
+                System.out.println("Press 1 to check balance, 2 to deposit money, 3 to withdraw money, 4 to change password");
+                int numb = inputComm.nextInt();
+
+                if (numb == 1){
+
+                    System.out.println("Your current balance is " + account1.balance);
+
+                }
+                if (numb == 2){
+
+                    System.out.println("How much would you like to deposit?");
+                    int deposit = input.nextInt();
+                    account1.balance = account1.balance + deposit;
+                    System.out.println("Your current balance is " + account1.balance);
+
+                }
+                if (numb == 3){
+
+                    System.out.println("How much would you like to withdraw?");
+                    int withdraw = input.nextInt();
+                    account1.balance = account1.balance - withdraw;
+                    System.out.println("Your current balance is " + account1.balance);
+
+                }
+                if (numb == 4) {
+
+                    System.out.println("To change your password, please enter your current password");
+                    String input2Password = input.nextLine();
+                    if (input2Password.equals(account1.password)) {
+
+                        System.out.println("Please enter your new password");
+                        account1.password = accountPassword.nextLine();
+
+                        System.out.println("You password has now been changed");
+
+                    }
+                    else{
+                        System.out.println("Password is incorrect");
+                    }
+
+                } 
+            }
+                else {
+                    System.out.println("Password is incorrect");
+                }
+
+            } 
+            else {
+                System.out.println("That is not a valad name");
+            }
+        }
+    }
 
